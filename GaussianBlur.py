bf88ab626867daa6cb0211f70f6b78a0dcdca0e3
lupsik1 <31741604+lupsik1@users.noreply.github.com>
2021-12-26 16:20:04 +0100

    gaussian blur almost done

--- GaussianBlur.py
+++ GaussianBlur.py
@@ -1,7 +1,18 @@
 import numpy as np
 from numpy.fft import fft2, ifft2
 from matplotlib import pyplot as plt
-from PIL import Image
+from PIL import Image, ImageOps
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+def padding(arr, xd, yd):
+    h = arr.shape[0]
+    w = arr.shape[1]
+    a = (xd - h) // 2
+    aa = xd - a - h
+    b = (yd - w) // 2
+    bb = yd - b - w
+    return np.pad(arr, pad_width=((a, aa), (b, bb)), mode='constant')
 
 
 # univariate normal distribution density
@@ -11,8 +22,14 @@ def dnorm(x, mu, sd):
 
 # using fft for 2d convolutions due to speed increase
 def fft_convolution(f1, f2):
+    print(f2)
+    f2 = padding(f2, f1.shape[0], f1.shape[1])
+    #print(f1)
+
     f1 = fft2(f1)
+
     f2 = fft2(np.flipud(np.fliplr(f2)))
+
     i, j = f1.shape
     cc = np.real(ifft2(f1 * f2))
     cc = np.roll(cc, -i / 2 + 1, axis=0)
@@ -29,7 +46,6 @@ def create_kernel(size):
 
     for i in range(size):
         kernel_1d[i] = dnorm(kernel_1d[i], 0, sigma)
-        print(kernel_1d[i])
     kernel = np.outer(kernel_1d.T, kernel_1d.transpose())
 
     kernel = np.divide(kernel, kernel.max())
@@ -38,12 +54,11 @@ def create_kernel(size):
 
 def gaussian_blur(img, kernel_size):
     kernel = create_kernel(kernel_size)
-    return fft_convolution(img, kernel)
+    return np.asarray(fft_convolution(img, kernel))
 
 
 img = Image.open("test.jpg")
-img.load()
+img = ImageOps.grayscale(img)
 data = np.asarray(img, dtype="int32")
-
-plt.imread(gaussian_blur(img, 8))
+plt.imread(gaussian_blur(np.asarray(img), 250))
 plt.imshow()
