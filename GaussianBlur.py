bb12696ef2db54d1157e25823345b4e2e12dd78c
lupsik1 <31741604+lupsik1@users.noreply.github.com>
2021-12-26 17:27:13 +0100

    gaussian blur almost done

--- GaussianBlur.py
+++ GaussianBlur.py
@@ -3,7 +3,10 @@ from numpy.fft import fft2, ifft2
 from matplotlib import pyplot as plt
 from PIL import Image, ImageOps
 import sys
+from numba import jit, njit
+
 np.set_printoptions(threshold=sys.maxsize)
+
 
 def padding(arr, xd, yd):
     h = arr.shape[0]
@@ -16,24 +19,24 @@ def padding(arr, xd, yd):
 
 
 # univariate normal distribution density
+@njit
 def dnorm(x, mu, sd):
     return 1 / (np.sqrt(2 * np.pi) * sd) * np.e ** (-np.power((x - mu) / sd, 2) / 2)
 
 
 # using fft for 2d convolutions due to speed increase
+
 def fft_convolution(f1, f2):
-    print(f2)
+    f1 = f1 / 255
     f2 = padding(f2, f1.shape[0], f1.shape[1])
-    #print(f1)
 
     f1 = fft2(f1)
-
     f2 = fft2(np.flipud(np.fliplr(f2)))
 
     i, j = f1.shape
     cc = np.real(ifft2(f1 * f2))
-    cc = np.roll(cc, -i / 2 + 1, axis=0)
-    cc = np.roll(cc, -j / 2 + 1, axis=1)
+    cc = np.roll(cc, -i // 2 + 1, axis=0)
+    cc = np.roll(cc, -j // 2 + 1, axis=1)
     return cc
 
 
@@ -54,11 +57,23 @@ def create_kernel(size):
 
 def gaussian_blur(img, kernel_size):
     kernel = create_kernel(kernel_size)
-    return np.asarray(fft_convolution(img, kernel))
+
+    img_rgb = np.asarray(np.split(img, 3, axis=-1)).squeeze()
+    rgb = [img_rgb[0], img_rgb[1], img_rgb[2]]
+    for i in range(3):
+        rgb[i] = fft_convolution(rgb[i], kernel)
+
+    for i in range(3):
+        rgb[i] = np.multiply(rgb[i], np.divide(255.0, rgb[i].max()))
+    final_image = np.moveaxis(np.asarray(rgb), 0, -1).astype(np.uint8)
+    return final_image
 
 
 img = Image.open("test.jpg")
-img = ImageOps.grayscale(img)
-data = np.asarray(img, dtype="int32")
-plt.imread(gaussian_blur(np.asarray(img), 250))
-plt.imshow()
+img = np.asarray(img)
+blur_kernel_size = 32
+img = gaussian_blur(img, kernel_size=blur_kernel_size)
+print(type(img))
+print(img.shape)
+pillow_image = Image.fromarray(img)
+pillow_image.show()
